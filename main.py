b257bff61b8deeb9e9fb45551b60d4ac0ec4c02c
ໂນອາ <109697122+Raumfahrzeug420@users.noreply.github.com>
2023-04-10 15:29:03 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
+import os
 import requests
 import discord
 from discord.ext import tasks, commands
 from Config import Token, Channel, RoleID, Seconds, Prefix
 intents = discord.Intents.all()
-client = commands.Bot(command_prefix=Prefix,intents=intents)
+client = commands.Bot(command_prefix=str(Prefix),intents=intents)
 
 print("Original Code Created by: MirayXS/Shin#6327 Edited by Pakyu!#6228")
 
@@ -16,28 +17,32 @@ async def on_ready():
         print(Error)
     print('<----------- NEW SESSION ----------->')
     print('[SUCCESS] : Logged in as ' + format(client.user))
-    print('-------------------------------------\n\n')
+    print('<----------------------------------->\n\n')
     await robloxgameclient_loop.start()
 
 # RobloxGameClient
 @tasks.loop(seconds=int(Seconds))
 async def robloxgameclient_loop():
+    print(f"Checking for Roblox Update...")
+    dir = os.path.dirname(os.path.realpath(__file__))
+    wdir = os.getcwd()
+    if dir != wdir:
+        os.chdir(dir)
     file = open("Version.txt", "r")
     fileread = file.readlines()
     oldData = "".join(str(x) for x in fileread)
-    newData = requests.get('http://setup.roblox.com/version') # This is the endpoint where Roblox updates their version number. This is new data.
-    print(f"Checking for Roblox Update...")
+    newData = requests.get('http://setup.roblox.com/version') # This is the endpoint where Roblox updates their version number. This is new data. 
     if oldData == "":
         print("[EMPTY] ( [X] ) Version.txt is Empty Will Set to the Latest Version.")
         with open("Version.txt", "w") as file:
-            file.write(str(newData.text))
-    elif str(newData.text) in oldData: # if the new data is the same as the old data...
+                file.write(str(newData.text))
+    elif str(newData.text) in str(oldData): # if the new data is the same as the old data...
         print("[CLEARED] ( [✓] ) No New RobloxGameClient Version!")
-    elif str(newData.text) not in oldData: # if the new data is **not** the same as the old data...
+    elif str(newData.text) not in str(oldData): # if the new data is **not** the same as the old data...
         print("[FAILED] ( [X] ) New RobloxGameClient Version!\n")
-        print("New RobloxGameClient Version: "+newData.text)
+        print("New RobloxGameClient Version: "+ newData.text)
         print("-------------------------------------")
-        print("Old RobloxGameClient Version: "+oldData)
+        print("Old RobloxGameClient Version: "+ oldData)
         print("-------------------------------------")
         channel = client.get_channel(int(Channel))
         if RoleID == "":
@@ -47,7 +52,7 @@ async def robloxgameclient_loop():
         else:
             await channel.send(f"||<@&{str(RoleID)}>|| \n```Roblox has been updated!!! \nPrevious Version: {oldData} \nUpdated Version: {newData.text}```")
         with open("Version.txt", "w") as file:
-             file.write(str(newData.text))
+            file.write(str(newData.text))
     file.close
 
 # Use this command to ping the bot, or to know if the bot crashed.  
