ea9418c2b42b351092aac36085f44748033da0c4
ໂນອາ <109697122+Raumfahrzeug420@users.noreply.github.com>
2023-04-10 15:08:48 -0400

    Update Config.py

--- Config.py
+++ Config.py
@@ -2,3 +2,4 @@ Token = "PASTE HERE" #Token
 Channel = "PASTE HERE" #Sends Message to Channel ID
 RoleID = "PASTE HERE" #Mentions a specific Role. If Empty will @everyone.
 Seconds = "600" #How many seconds till it checks if Roblox Updated.
+Prefix = "" #Prefix for commands
